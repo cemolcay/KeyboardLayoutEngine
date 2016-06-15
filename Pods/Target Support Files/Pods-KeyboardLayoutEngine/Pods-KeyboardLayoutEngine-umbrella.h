947af113e297884a9aa164a47946af8a7c8bcda3
cemolcay <ccemolcay@gmail.com>
2016-06-15 12:41:41 -0700

    Remove ManualLayout

---
+++ Pods/Target Support Files/Pods-KeyboardLayoutEngine/Pods-KeyboardLayoutEngine-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_KeyboardLayoutEngineVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_KeyboardLayoutEngineVersionString[];
+
---
+++ Pods/Target Support Files/Pods-KeyboardLayoutEngineTests/Pods-KeyboardLayoutEngineTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_KeyboardLayoutEngineTests : NSObject
+@end
+@implementation PodsDummy_Pods_KeyboardLayoutEngineTests
+@end
